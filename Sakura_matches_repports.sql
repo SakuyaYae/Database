760de6ead2d2f9c82f3bb6f906f9cff6c3607e82
SakuyaYae <darkreimu@outlook.com>
2021-11-25 11:53:23 +0100

    Update Sakura_matches_repports.sql

--- Sakura_matches_repports.sql
+++ Sakura_matches_repports.sql
@@ -1,4 +1,18 @@
 SELECT * FROM team ORDER BY namn;
 SELECT * FROM game ORDER BY id;
 
-SELECT team.id as "Team", namn, sum(points_home) as "points home", sum(points_away) as "points away", sum(points_home + points_away) as "total" FROM team, game WHERE team.id like 3 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_home) as "points home" FROM team, game WHERE home = 1 and team.id = 1 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_away) as "points away" FROM team, game WHERE away = 1 and team.id = 1 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_home + points_away) as "points home" FROM team, game WHERE home = 1 and team.id = 1 group by namn ORDER BY namn;
+
+SELECT team.id as "Team", namn, sum(points_home) as "points home" FROM team, game WHERE home = 2 and team.id = 2 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_away) as "points away" FROM team, game WHERE away = 2 and team.id = 2 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_home + points_away) as "points home" FROM team, game WHERE home = 2 and team.id = 2 group by namn ORDER BY namn;
+
+SELECT team.id as "Team", namn, sum(points_home) as "points home" FROM team, game WHERE home = 3 and team.id = 3 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_away) as "points away" FROM team, game WHERE away = 3 and team.id = 3 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_home + points_away) as "points home" FROM team, game WHERE away = 3 and team.id = 3 group by namn ORDER BY namn;
+
+SELECT team.id as "Team", namn, sum(points_home) as "points home" FROM team, game WHERE home = 4 and team.id = 4 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_away) as "points away" FROM team, game WHERE away = 4 and team.id = 4 group by namn ORDER BY namn;
+SELECT team.id as "Team", namn, sum(points_home + points_away) as "points total" FROM team, game WHERE away = 4 and team.id = 4 group by namn ORDER BY namn;
